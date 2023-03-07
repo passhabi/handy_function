16949a46bc88ae9f064522936f06f4ee70b6fd22
Pedram (Hussein) Asshabi <pedramas@live.com>
2023-03-08 02:59:42 +0430

    Add files via upload

--- moving_average_biased_correction.py
+++ moving_average_biased_correction.py
@@ -3,7 +3,7 @@ import numpy as np
 
 shape = [500]
 
-data = np.random.randint(-100, 100, shape)
+data = np.random.randint(0, 100, shape)
 m_avg = np.zeros(shape)  # store means computed by moving average
 no_biased_m_avg = np.zeros(shape)  # store means computed by corrected bias moving average
 normal_avg = np.zeros(shape)  # store means computed by normal averaging
@@ -12,18 +12,17 @@ vt, t = 0, 0
 beta = 0.98
 
 
-def no_bias_mv(number):
-    global vt, t
+for number in data:
 
     t += 1
+    # Exponentially weighted Moving Average:
     vt = vt * beta + (1 - beta) * number
-    no_biased_m_avg[t - 1] = (vt / (1 - (beta ** t)))
+    # Corrected biased Moving Average:
+    no_biased_m_avg[t - 1] = vt / (1 - (beta ** t))
+    # update other tracking arrays:
     m_avg[t - 1] = vt
-    normal_avg[t-1] = np.sum(data[:t]) / t
+    normal_avg[t - 1] = np.sum(data[:t]) / t
 
-
-for num in data:
-    no_bias_mv(num)
 
 # plt.scatter(range(len(data)), data, marker='.', color='pink')
 plt.plot(normal_avg, label="Normal Average")
@@ -31,3 +30,5 @@ plt.plot(m_avg, label="Exponentially weighted Moving Average")
 plt.plot(no_biased_m_avg, label="Corrected biased Moving Average")
 plt.legend()
 plt.show()
+
+#%%
