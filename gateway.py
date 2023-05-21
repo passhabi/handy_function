c507c9f696b5a7c0f99c9fd9fcb61fa42bb087b9
Pedram <pedramas@live.com>
2023-05-21 10:04:17 +0330

    Add the gateway.py file. This file will do the operation on login.

---
+++ gateway.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+username = input("Enter your user name:")
+password = input("Enter your password:")
+
+print("Welcome ", username)
+print("An error occuerd!") # data should be checked with datebase.
+sys.exit(-1)
